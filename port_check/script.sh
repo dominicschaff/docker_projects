f52ce2779e08e58704e0ca50e9f336e52e23c8b9
Dominic Schaff <dominic@schaff.dev>
2021-01-16 08:20:43 +0200

    Update for shorthand

--- port_check/script.sh
+++ port_check/script.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+if [ ! -z "$CHECK"  ]; then
+  CHECK_HOST="$(echo "$CHECK" | cut -d':' -f1)"
+  CHECK_PORT="$(echo "$CHECK" | cut -d':' -f2)"
+fi
+
 if [ -z "$CHECK_HOST" ]; then
-  echo "CHECK_HOST env must be set"
+  echo "CHECK_HOST env must be set, or if using CHECK format should be 'CHECK=host:port'"
   exit 1
 fi
 
 if [ -z "$CHECK_PORT"  ]; then
-  echo "CHECK_PORT env must be set"
+  echo "CHECK_PORT env must be set, or if using CHECK format should be 'CHECK=host:port'"
   exit 1
 fi
 
